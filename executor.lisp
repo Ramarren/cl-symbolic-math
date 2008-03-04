bd15962ab11cc2e8c05166d2492ab72efa1a38be
Ramarren <ramarren@gmail.com>
2008-03-04 12:46:07 +0100

    Macro for binary operators.

--- executor.lisp
+++ executor.lisp
@@ -1,3 +1,37 @@
 (in-package :symbolic)
 
 ;;;; Execute symbolic math trees. Or even better, compile them to closures.
+
+(def! symbolic-compile nil
+  (constantly nil))
+
+;;; terminals
+
+(def symbolic-compile _x
+  (where (symbolp _x))
+  (lambda (b)
+    (assoc _x b)))
+
+(def symbolic-compile _n
+  (where (numberp _n))
+  (constantly _n))
+
+;;; basic arithmetic
+(defmacro define-binary-operator (symbolic-op real-op)
+  (with-unique-names (b l r)
+   `(def symbolic-compile (,symbolic-op _a _b)
+      (let ((,l (symbolic-compile _a))
+	    (,r (symbolic-compile _b)))
+       (lambda (,b)
+	 (,real-op (funcall ,l ,b)
+		   (funcall ,r ,b)))))))
+
+(def symbolic-compile (+ _a _b)
+  (lambda (b)
+    (+ (funcall (symbolic-compile _a) b)
+       (funcall (symbolic-compile _b) b))))
+
+(def symbolic-compile (- _a _b)
+  (lambda (b)
+    (- (funcall (symbolic-compile _a) b)
+       (funcall (symbolic-compile _b) b))))

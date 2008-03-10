3da6a8a8fb13c778f4d2108b6cac4c6e729d5da8
Ramarren <ramarren@gmail.com>
2008-03-10 21:04:46 +0100

    Add derivatives for basic arithmetic operators.

--- derivate.lisp
+++ derivate.lisp
@@ -8,15 +8,39 @@
   nil)
 
 (def symbolic-derive _x
+  (where (or (numberp _x)
+	     (member _x *constants*)))
+  0)
+
+(def symbolic-derive _x
   (where (symbolp _x))
   (if (eql *derivation-variable* _x)
       1
       _x))
 
-(def symbolic-derive _x
-  (where (or (numberp _x)
-	     (member _x *constants*)))
-  0)
-
 (def symbolic-derive (+ _a _b)
-  (let ((a (symbolic-derive)))))
+  (let ((a (symbolic-derive _a))
+	(b (symbolic-derive _b)))
+    (list '+ a b)))
+
+(def symbolic-derive (- _a)
+  (list '- (symbolic-derive _a)))
+
+(def symbolic-derive (- _a _b)
+  (let ((a (symbolic-derive _a))
+	(b (symbolic-derive _b)))
+    (list '- a b)))
+
+(def symbolic-derive (* _a _b)
+  (let ((a (symbolic-derive _a))
+	(b (symbolic-derive _b)))
+    (list '+
+	  (list '* _a b)
+	  (list '* a _b))))
+
+(def symbolic-derive (/ _a _b)
+  (let ((a (symbolic-derive _a))
+	(b (symbolic-derive _b)))
+    `(/ (+ (* ,a ,_b)
+	   (* ,_a ,b))
+	(^ ,b 2))))
